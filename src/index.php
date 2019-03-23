57349ef8c6193a4065934da478a0e162787aa18e
Jyrone Parker <jyrone.parker@gmail.com>
2019-03-23 14:33:13 -0400

    Update index.php

--- src/index.php
+++ src/index.php
@@ -14,7 +14,8 @@
  * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
  * copies of the Software, and to permit persons to whom the Software is
  * furnished to do so, subject to the following conditions:
- *
+date_default_timezone_set('America/New_York'); // Use your own timezone string.
+*
  * The above copyright notice and this permission notice shall be included in
  * all copies or substantial portions of the Software.
  *
